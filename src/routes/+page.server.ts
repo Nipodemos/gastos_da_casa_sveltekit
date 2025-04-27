67fcf17fe52370f145accf56fa5e41550f8295d4
Nipodemos <nipodemos13@gmail.com>
2025-04-27 00:29:03 -0300

    remove código de criação de instrumento

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,7 @@ import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
 	const { data } = await supabase.from('instruments').select();
-	const result = await supabase.from('instruments').insert([{ name: 'guitar' }]);
-	console.log('result', result);
+
 	return {
 		instruments: data ?? []
 	};

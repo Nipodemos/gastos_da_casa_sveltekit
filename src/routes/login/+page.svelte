0e3c573a37f3553a6e1958d545798b42e26c8540
Nipodemos <nipodemos13@gmail.com>
2025-06-02 19:24:30 -0300

    começo da tela de login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { Col, Container, Form, FormGroup, Input, Row } from '@sveltestrap/sveltestrap';
+
+	let dadosFormulario = $state({
+		login: {
+			status: '',
+			feedback: '',
+			value: ''
+		},
+		senha: {
+			status: '',
+			feedback: '',
+			value: ''
+		}
+	});
+</script>
+
+<Container sm>
+	<Row>
+		<Col sm={{ size: 4, offset: 4 }}>
+			<Form>
+				<FormGroup floating label="Email*">
+					<Input
+						type="email"
+						placeholder="Email"
+						name="login"
+						valid={dadosFormulario.login.status === 'ok'}
+						invalid={dadosFormulario.login.status === 'error'}
+						feedback={dadosFormulario.login.feedback}
+						bind:value={dadosFormulario.login.value}
+					/>
+				</FormGroup>
+				<FormGroup floating label="Senha*">
+					<Input
+						type="password"
+						placeholder="Senha"
+						name="senha"
+						valid={dadosFormulario.senha.status === 'ok'}
+						invalid={dadosFormulario.senha.status === 'error'}
+						feedback={dadosFormulario.senha.feedback}
+						bind:value={dadosFormulario.senha.value}
+					/>
+				</FormGroup>
+			</Form>
+		</Col>
+	</Row>
+</Container>

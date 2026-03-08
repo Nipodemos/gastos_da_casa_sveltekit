54123fe0cbb9f981044f3028b29c550dc4463c77
Nipodemos <nipodemos13@gmail.com>
2026-03-08 09:51:23 -0300

    converte hash-password para js puro para poder rodar com node

--- hash-password.js
+++ hash-password.js
@@ -3,11 +3,11 @@ import { randomBytes, scryptSync } from 'node:crypto';
 const senha = process.argv[2];
 
 if (!senha) {
-	console.error('Uso: ts-node hash-password.ts <senha>');
+	console.error('Uso: node hash-password.js <senha>');
 	process.exit(1);
 }
 
 const salt = randomBytes(16).toString('hex');
 const hash = scryptSync(senha, salt, 64).toString('hex');
 
-console.log(`${salt}:${hash}`);
+process.stdout.write(salt + ':' + hash + '\n');
